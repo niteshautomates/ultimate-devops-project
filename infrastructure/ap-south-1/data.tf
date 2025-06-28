9c40c6d34762f0503505c8a3aea84b1309d46512
nitesh wayafalkar <niteshwayafalkar@yahoo.com>
2025-06-29 00:08:57 +0530

    feat: added caller identity to read info about account

---
+++ infrastructure/ap-south-1/data.tf
@@ -0,0 +1,9 @@
+# Data sources for the SRE Challenge 2025 infrastructure
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
