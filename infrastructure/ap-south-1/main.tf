a95f96848ba71c708935ab49629e9e9adcf71880
nitesh wayafalkar <niteshwayafalkar@yahoo.com>
2025-06-29 02:12:03 +0530

    feat: added IAM module to main.tf

--- infrastructure/ap-south-1/main.tf
+++ infrastructure/ap-south-1/main.tf
@@ -6,12 +6,32 @@ locals {
 }
 
 
+# Create IAM Roles
+
+module "IAM" {
+  source = "../modules/IAM"
+   assume_role_policy = {
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com" # Example: Allow EC2 to assume this role
+        }
+        Action = "sts:AssumeRole"
+      },
+    ]
+  }
+  tags = local.common_tags
+}
+# Create KMS Keys
 module "kms" {
   source       = "../modules/kms"
   tags         = local.common_tags
   project_name = var.project_name
 }
 
+# Create S3 buckets for storing statefiles and locking
 module "s3" {
   source             = "../modules/S3"
   tags               = local.common_tags
