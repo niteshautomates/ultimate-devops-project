db0ed665149cd76404f52dcd76e0689eba1e493c
Nitesh Wayafalkar <niteshwayafalkar@yahoo.com>
2025-07-03 14:25:31 +0530

    removed region to provider block

--- infrastructure/ap-south-1/provider.tf
+++ infrastructure/ap-south-1/provider.tf
@@ -3,13 +3,11 @@ terraform {
     aws = {
         source  = "hashicorp/aws"
         version = "~> 5.0"
-        region = "ap-south-1"
 
     }
     random = {
         source  = "hashicorp/random"
         version = "~> 3.0"
-      region = "ap-south-1"
     }
   }
 }
